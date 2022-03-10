d52dbc42c0baf732e8c210d7328fb4dd4093f9bb
Op-If <OsatuyiIfe@gmail.com>
2022-03-10 10:31:25 -0800

    C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - print a sentence followed by a new line
  *
- * Return: returns 0 after printing
+ * Return: returns 1 after printing
  */
 
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	return (1);
 }
