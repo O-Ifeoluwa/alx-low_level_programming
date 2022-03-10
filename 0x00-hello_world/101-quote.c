df30e161f398f4db51791f1be1f1b542e0aecd1b
Op-If <OsatuyiIfe@gmail.com>
2022-03-10 10:28:29 -0800

    C programming with ALX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
  * main - print a sentence followed by a new line
  *
- * Return: returns 1 after printing
+ * Return: returns 0 after printing
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-	return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (0);
 }
