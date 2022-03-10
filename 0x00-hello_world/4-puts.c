097eb38ca0b3995ed733b9506b245622d2c99b85
Op-If <OsatuyiIfe@gmail.com>
2022-03-10 09:12:06 -0800

    C programming with ALX

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#!/bin/bash
+
 #include <stdio.h>
 
 
@@ -18,19 +18,8 @@
 
 
 int main(void)
-
-
-
 {
 
-
-
-			puts("\"Programming is like building a multilingual puzzle");
-
-
-
-						return (0);
-
-
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
