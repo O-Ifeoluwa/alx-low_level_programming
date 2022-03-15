3535f5a99470eb4374649ac49a4c09ba4d096001
Op-If <OsatuyiIfe@gmail.com>
2022-03-15 16:01:24 -0700

    C functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int starter = 0;
+	char letter = 'a';
+
+	while (starter < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+
+		starter++;
+	}
+}
