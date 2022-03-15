50f595f77db9dddb0b5c4eb9d856214f699ab670
O-Ifeoluwa <horshatuhyi@gmail.com>
2022-03-15 22:35:47 +0100

    C tasks

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,13 @@
 
 int main(void)
 {
-_putchar('_')
-_putchar('p')
-_putchar('u')
-_putchar('t')
-_putchar('c')
-_putchar('h')
-_putchar('a')
-_putchar('r')
-_putchar('\n')
-return (0);
+	char grotto[9] = "_putchar";
+	int i = 0;
+
+	for (1 = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n')
+	return (0);
 }
