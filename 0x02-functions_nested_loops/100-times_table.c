0a159acdd2bd3d24108585e770cc4fa2735d2b44
Op-If <OsatuyiIfe@gmail.com>
2022-03-16 07:09:30 -0700

    fixed problem with compiling code

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- *print_times_table - prints the times table
- *@n: integer for which the times table will be printed
+ * print_times_table - prints the times table
  *
- *Description: prints the times table
+ * @n: is an integer
+ *
+ * Description: prints the times table
  *
  * Return: void
  */
