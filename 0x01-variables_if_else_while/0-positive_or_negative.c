376e7a472fb1a9aaa639e5ccd86587ba74bb20b5
O-Ifeoluwa <horshatuhyi@gmail.com>
2022-03-20 18:26:34 +0100

    overdue task on C variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,13 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+/**
+ * main - beginning of program
+ * 
+ * Description: checks if n is positive, negative or zero
+ * Return: 0 on success
+ */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
