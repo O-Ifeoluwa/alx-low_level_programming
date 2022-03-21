0768fe80df0e5a6a70b35cf6bee42d86bdd9ddf8
O-Ifeoluwa <horshatuhyi@gmail.com>
2022-03-21 20:06:58 +0100

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98.c - function that takes a pointer to an int as parameter and updates the value it points to to 98.
+ * reset_to_98 - changes the value of n to p
  * Description: function that takes a pointer to an int as parameter and updates the value it points to to 98.
  * @n: an integer
  * @*p: a pointer to n
