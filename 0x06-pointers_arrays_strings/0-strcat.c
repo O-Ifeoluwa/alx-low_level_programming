34dfd55a7e9b9ca6ef3112ee05e487f761b228f6
O-Ifeoluwa <horshatuhyi@gmail.com>
2022-03-23 21:21:49 +0100

    pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,13 +14,14 @@ char *_str(char *dest, char *src)
 	int d = 0;
 	int s = 0;
 
-	for (d = 0; dest[d] != "\0"; d++);
-	for (s = 0; src[s] != "\0"; s++)
+	while (dest[d] && dest[d] != '\0')
+		d++;
+	while (src[s])
 	{
 		dest[d] = src[s];
 		d++;
+		s++;
 	}
-
-	dest[d] = "\0";
+	dest[d] = '\0';
 	return (dest);
 }
