f74161f0242d5d107ad28bd9533d825d9f35418b
O-Ifeoluwa <horshatuhyi@gmail.com>
2022-03-23 21:42:30 +0100

    sigh

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (d = 0; dest[d] != '\0'; d++)
 		;
-	for (s = 0; src[s] != '\0' && n > 0; c++, n--, s++)
+	for (s = 0; src[s] != '\0' && n > 0; s++, n--, s++)
 	{
 		dest[d] = src[s];
 	}
