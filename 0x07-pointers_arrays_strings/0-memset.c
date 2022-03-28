fb570001213c1a8f04a2339d486c2fc62ffd32ce
O-Ifeoluwa <horshatuhyi@gmail.com>
2022-03-28 23:42:54 +0100

    pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @n: byte
+ * @s: pointer to memory area
+ * Return: 0 on success
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	unsigned int *s;
+
+	i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (0);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+more tasks with pointers, arrays and strings
