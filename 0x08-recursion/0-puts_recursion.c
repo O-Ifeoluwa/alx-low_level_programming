b4a8fc5dcf8da3f0ed4599842895719647c97f61
O-Ifeoluwa <horshatuhyi@gmail.com>
2022-03-30 23:06:48 +0100

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  _puts_recursion - prints a string, followed by a new line
+ *  @s: a string
+ *  Return: string?
+ */
+
+void _puts_recursion(char *s)
+{
+	do
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	} while ( *s != '\0')
+	_putchar('\n');
+}
