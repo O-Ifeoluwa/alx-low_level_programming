9b6905f17eea03a12017c1b222574a8c5eddff34
O-Ifeoluwa <horshatuhyi@gmail.com>
2022-04-03 02:57:04 +0100

    sigh

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,20 +11,29 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum, cen;
-	sum = 0;
+	int i, j, add = 0;
 
-	if (argc == 1)
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (argv[i][j] < 48 || argv[i][j] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			add += atoi(argv[i]);
+		}
+
+		printf("%d\n", add);
+	}
+	else if (argc == 1)
 	{
 		printf("0\n");
-		return (0);
 	}
-	for (i = 1; i < argc; i++)
-	{
-		cen = atoi(argv[i]);
-		if (cen >= 0)
-			sum += cen;
-	}
-	printf("%d\n", sum);
 	return (0);
 }
