d6b9de5e6e64a78269a397cd34ee11f526a071c0
O-Ifeoluwa <horshatuhyi@gmail.com>
2022-04-03 01:54:14 +0100

    sigh

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#define UNUSED(x) (void)(x)
 
 /**
  * main - program prints its name ffed by a new line
@@ -8,8 +9,9 @@
  * Return: 0 on success
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
+	UNUSED(argc);
 	printf("Name of program is: %s\n", argv[0]);
 	return (0);
 }
