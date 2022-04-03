91d0ff831bfc5a01d585b834a3ae765081b8c1a4
Op-If <OsatuyiIfe@gmail.com>
2022-04-02 17:32:04 -0700

    sigh

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program prints its name ffed by a new line 
+ * main - program prints its name ffed by a new line
  * @argc: first argument
  * @argv: second argument
  * Return: 0 on success
