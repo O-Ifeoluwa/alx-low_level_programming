42a6b0f049bd38e212d4f3966906a51daeb95d6d
O-Ifeoluwa <horshatuhyi@gmail.com>
2022-04-03 01:21:44 +0100

    ArgC and ArgV

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - body of program
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("Name of program is: %s\n", argv[0]);
+	return (0);
+}
