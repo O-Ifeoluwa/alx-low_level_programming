b790f91bc48a7df67de4070e159aa2596c576721
O-Ifeoluwa <horshatuhyi@gmail.com>
2022-04-07 22:47:53 +0100

    function returns a pointer

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - function allocates memory and returns a pointer
+ * @b: is an unsigned b
+ * Return: 0 on success and 98 on failure
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return(ptr);
+}
