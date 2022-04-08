8e3d8b4aec27b0f369518654cd7bbdc65b42be8b
O-Ifeoluwa <horshatuhyi@gmail.com>
2022-04-08 23:09:55 +0100

    sigh

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,13 +11,8 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
-	unsigned int str1;
-	unsigned int str2;
-	unsigned int i;
-
-	unsigned int str1 = 0;
-	unsigned int str2 = 0;
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -25,33 +20,33 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[str1] != '\0')
+	while (s1[size1] != '\0')
 	{
-		++str1;
+		size1++;
 	}
 
-	while (s2[str2] != '\0')
+	while (s2[size2] != '\0')
 	{
-		++str2;
+		size2++;
 	}
 
-	if (n > str2)
-		n = str2;
-	s = malloc((str1 + n + 1) * sizeof(char));
+	if (n > size2)
+		n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
 
-	if (s == NULL)
+	if (p == NULL)
 		return (0);
 
-	for (i = 0; i < str1; i++)
+	for (i = 0; i < size1; i++)
 	{
-		s[i] = str1[i];
+		p[i] = s1[i];
 	}
 
-	for (; i < (str1 + n); i++)
+	for (; i < (size1 + n); i++)
 	{
-		s[1] = s2[i - str1];
+		p[i] = s2[i - size1];
 	}
-	s[i] = '\0';
+	p[i] = '\0';
 
-	return (s);
+	return (p);
 }
